498408fc61445b8ee7c64bf2d4c7783967ec73d4
JonasQiu <long229260097@qq.com>
2020-05-26 05:24:40 +0800

    search模块的清除功能

--- pages/components/search/channel/noContent/noContent.js
+++ pages/components/search/channel/noContent/noContent.js
@@ -12,13 +12,29 @@ Component({
    */
   data: {
     hotList:['shuai','sad','asdasd','shuai','sad','asdasd','shuai','sad','asdasd'], //数据截取前10个
-    historyList:['shuai','sad','asdasd','shuai','sad','asdasd'] //数据截取前8个
+    historyList:['s','shuai','sad','asdasd','shuai','sad','asdasd','shuai'], //数据截取前8个
+    showClear:true
   },
-
+  // show(e){  未能实现，没有历史数据时，初始化不显示
+  //   if(this.data.historyList === []){
+  //     this.setData({
+  //       showClear:false
+  //     })
+  //   }
+  // },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    clearHistory(e){
+      this.setData({
+        historyList:[],
+        showClear:false
+      })
+      wx.showToast({
+        title: '清除成功',
+        icon:"success"
+      })
+    }
   }
 })
