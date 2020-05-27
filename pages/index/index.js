4b85a3f1833e25e4ca16fb44f21d3e07706ad263
LiuHai <1711323390@qq.com>
2020-05-27 15:01:32 +0800

    机构搜索查询模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,15 @@
 //获取应用实例
 const app = getApp()
-let userInfo={};
+let userInfo = {};
 
 Page({
   data: {
-    askNum:20,
+    askNum: 20,
     PageCur: 'homePage',
-    position:'英德',
-    weather:'阴',
-    degree:"26",
-    hotSearch:"王后雄(满1000减100)"
+    position: '英德',
+    weather: '阴',
+    degree: "26",
+    hotSearch: "王后雄(满1000减100)"
   },
 
   // 底部导航切换
@@ -19,7 +19,5 @@ Page({
     })
   },
   // 加载页面立刻执行
-  onLoad: function () {
-   
-  }
+  onLoad: function () {}
 })
