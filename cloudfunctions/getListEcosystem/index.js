6872abf7d761f7c6376bb477f74c9266c873134b
LiuHai <1711323390@qq.com>
2020-06-02 15:15:42 +0800

    UserJs
    一、新增User文件夹的两个JS文件
    1、UserToOrg是用户对机构的操作
    2、UserToEco是用户对生态圈的操作
    
    二、新增获取数据时的字段
    1、获取机构信息时新增isCollect字段判断该用户是否收藏了该机构，值为布尔值
    2、获取生态圈信息是新增isLike字段判断该用户是否点赞了该机构，值为布尔值

--- cloudfunctions/getListEcosystem/index.js
+++ cloudfunctions/getListEcosystem/index.js
@@ -7,30 +7,11 @@ const _ = db.command
 // 云函数入口函数
 exports.main = function main(event, context) {
     return new Promise(async (resolve, reject) => {
-        let {
-            startNum,
-            Num,
-        } = event;
-        let totalNum = await db.collection('Eco').count();
-        let isBottom = startNum + Num >= totalNum.total;
-        db.collection('Eco').skip(startNum).limit(Num).get().then(async (res) => {
-            let ecoList = res.data
-            if (ecoList.length < 1) {
-                resolve({
-                    ecoList,
-                    isBottom: true,
-                })
-            }
-            for (let i = 0; i < ecoList.length; i++) {
-                ecoList[i]['orgInfo'] = (await db.collection('Org').doc(ecoList[i]['orgInfo']['orgId']).get()).data
-                ecoList[i]['userInfo'] = (await db.collection('User').doc(ecoList[i]['userInfo']['userId']).get()).data
-            }
-            resolve({
-                ecoList,
-                isBottom
-            })
-        }).catch(res => {
-            reject(res)
-        })
+        let ecoList = event.ecoList;
+        for (let i = 0; i < ecoList.length; i++) {
+            ecoList[i]['orgInfo'] = (await db.collection('Org').doc(ecoList[i]['orgInfo']['orgId']).get()).data
+            ecoList[i]['userInfo'] = (await db.collection('User').doc(ecoList[i]['userInfo']['userId']).get()).data
+        }
+        resolve(ecoList)
     })
 }
