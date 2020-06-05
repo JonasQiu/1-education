bb53c2af1271d9897b6ccba1f6eba54ffd58c34d
JonaQiu <long229260097@qq.com>
2020-06-05 21:54:31 +0800

    orgDetail

--- pages/components/orgDetail/info/info.js
+++ pages/components/orgDetail/info/info.js
@@ -12,25 +12,13 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {
-    cur: ""
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
   methods: {
-    attentionTap(e) {
-      if (this.data.cur === '') {
-        this.setData({
-          cur: "attention"
-        })
-      } else {
-        this.setData({
-          cur: ""
-        })
-      }
-    },
+
     lookMap(e) {
       //传终点的纬度经度的参数过去，通过onload获得,showNav判断到达的页面是否，yes进行导航的功能还是no只是展示地图
       wx.navigateTo({
